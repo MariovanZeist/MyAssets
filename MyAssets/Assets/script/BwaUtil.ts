a414b1cc635095862a76e0409898774453db2acb
Mario van Zeist <m.vanzeist@home.nl>
2022-06-28 20:42:14 +0200

    Add project files.

---
+++ MyAssets/Assets/script/BwaUtil.ts
@@ -0,0 +1,35 @@
+﻿export class BwaUtil {
+
+    static AdjustClass(element: HTMLElement, classNames: string) {
+        let classArray: (string)[] = [];
+        let addClass: boolean = true;
+
+        const adjust = () => {
+            if (classArray.length) {
+                classArray.forEach(item => addClass ? element.classList.add(item) : element.classList.remove(item));
+                classArray = [];
+            }
+        }
+
+        for (let className of classNames.split(" ")) {
+            if (className && className.length > 0) {
+                let isRemove = className[0] == '-'
+                if (isRemove || className[0] == '+') {
+                    className = className.slice(1);
+                }
+
+                if (!isRemove != addClass) {
+                    adjust();
+                    addClass = !isRemove;
+                }
+                classArray.push(className);
+            }
+        }
+        adjust();
+    }
+
+    public AdjustClass(element: HTMLElement, classNames: string) {
+        BwaUtil.AdjustClass(element, classNames);
+    }
+}
+(window as any).BwaUtil = new BwaUtil();  
---
+++ MyAssets/Assets/webpack.config.js
@@ -0,0 +1,54 @@
+﻿const TerserJsPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const RemovePlugin = require('remove-files-webpack-plugin');
+
+module.exports = (env, args) => ({
+    resolve: { extensions: ['.scss', '.ts', '.js'] },
+    devtool: args.mode === 'development' ? 'source-map' : false,
+
+    entry: {
+        'BwaBlazor': './script/BwaUtil.ts',
+        'components.bwa': './css/components.bwa.scss'
+    },
+    module: {
+        rules: [
+            { test: /\.ts?$/, exclude: /node_modules/ , loader: 'ts-loader' },
+            { test: /\.(s(a|c)ss)$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] }
+        ],
+    },
+    optimization: {
+        sideEffects: true,
+        concatenateModules: true,
+        providedExports: true,
+        usedExports: true,
+        innerGraph: true,
+        minimize: true,
+        minimizer: [new TerserJsPlugin({
+            terserOptions: {
+                ecma: 2019,
+                compress: {
+                    passes: 3
+                },
+                mangle: {
+                },
+                module: false,
+                format: {
+                    ecma: 2019
+                },
+                keep_classnames: false,
+                keep_fnames: false,
+                toplevel: true
+            }
+        })]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+        }),
+        new RemovePlugin({
+            after: {
+                include: ['./wwwroot/components.bwa.js', './wwwroot/components.bwa.js.map']
+            }
+        })
+    ],
+})
